3fca47018c52240544d4eaa626fdce24837bc9b9
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:22:13 +0200

    core: Include GZOSP pathmap
    Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,3 +93,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(GZOSP_BUILD),)
+-include $(TOPDIR)vendor/gzosp/build/core/pathmap.mk
+endif
