045003c08918b6680fecd0c6f8fea9bc9dac054c
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    soong_config: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Author: Stricted <info@stricted.net>
    Date:   Sat Feb 17 17:28:52 2018 +0100
    
        soong_config: Replace space with colon on TARGET_LD_SHIM_LIBS
    
        that way we can split the shim list logically into multiple lines like
    
        TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
            /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
            /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
    
        to not have one single huge line
    
        Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf
    
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Thu Mar 8 22:34:14 2018 +0100
    
        soong: Forward-port to AOSP P
    
        Change-Id: Ibec6ec41ff0ab2c67a81cbfd52cf71acbddafd32
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -109,6 +109,8 @@ $(call add_json_bool, NativeCoverage,                    $(filter true,$(NATIVE_
 $(call add_json_list, CoveragePaths,                     $(COVERAGE_PATHS))
 $(call add_json_list, CoverageExcludePaths,              $(COVERAGE_EXCLUDE_PATHS))
 
+$(call add_json_str,  Target_shim_libs,                  $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+
 $(call add_json_bool, ArtUseReadBarrier,                 $(call invert_bool,$(filter false,$(PRODUCT_ART_USE_READ_BARRIER))))
 $(call add_json_bool, Binder32bit,                       $(BINDER32BIT))
 $(call add_json_bool, Brillo,                            $(BRILLO))
