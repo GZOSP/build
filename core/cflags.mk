0124df176df1f980988caccc5e64913d1684a5cb
theringmaster <gzroms@hotmail.com>
2018-10-19 20:15:19 -0400

    DO NOT MERGE: Add seperate cflags makefile
    Till we sort out repos
    
    Change-Id: I6cfb4ecb4ee7da19aa045158cde4fdfac76a6fa9

---
+++ core/cflags.mk
@@ -0,0 +1,12 @@
+# Temporary Fix to ignore errors till repos have fixes for these
+my_cflags += -Wall -Werror -Wno-unused-variable -Wno-unused-value -Wno-unused-function \
+             -Wno-unused-label -Wno-user-defined-warnings -Wno-unused-parameter -Wno-char-subscripts \
+             -Wno-string-plus-int -Wno-sometimes-uninitialized -Wno-format-extra-args -Wno-format \
+             -Wno-parentheses-equality -Wno-macro-redefined -Wno-logical-op-parentheses \
+             -Wno-parentheses -Wno-unused-private-field -Wno-missing-braces -Wno-implicit-function-declaration \
+             -Wno-pointer-arith -Wno-gnu-designator -Wno-constant-logical-operand -Wno-incompatible-pointer-types \
+             -Wno-address-of-packed-member -Wno-enum-conversion -Wno-self-assign \
+             -Wno-parentheses -Wno-unused-private-field -Wno-missing-braces \
+             -Wno-implicit-function-declaration -Wno-mismatched-tags -Wno-sign-compare -Wno-sign-conversion \
+             -Wno-reorder -Wno-null-conversion -Wno-overloaded-virtual -Wno-writable-strings -Wno-float-conversion \
+             -Wno-null-arithmetic -Wno-conversion -Wno-missing-field-initializers -Wno-switch -Wno-sizeof-array-argument
