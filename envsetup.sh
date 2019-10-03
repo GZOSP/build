38217aa5dbbb2ed8cb92c88e4e6caf6d401205ee
Dan Willemsen <dwillemsen@google.com>
2019-10-03 15:13:12 -0700

    Fix lunch choices w/zsh
    zsh doesn't split variables (by default), but it will split command
    output. bash will split both. Apparently a portable way to do this in
    both is to just turn it into another command: "$(echo $var)".
    
    Test: source build/envsetup.sh; lunch (see list; repeat in bash and zsh)
    Change-Id: I9716a8d77aefdf05da8923deb9e37e0b6132b549

--- envsetup.sh
+++ envsetup.sh
@@ -615,7 +615,7 @@ function print_lunch_menu()
 
     local i=1
     local choice
-    for choice in $choices
+    for choice in $(echo $choices)
     do
         echo " $i. $choice "
         i=$(($i+1))
